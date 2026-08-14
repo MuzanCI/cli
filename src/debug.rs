5d96c1a3701047cfae192e3c627afa6f25970737
Bobby Chen <cb168.github@fastmail.com>
2026-08-13 17:46:33 -0700

    Add debugclient requests

--- src/debug.rs
+++ src/debug.rs
@@ -1,7 +1,10 @@
 use std::path::PathBuf;
+use std::process::ExitCode;
+use std::process::ExitStatus;
 
 use http::Request;
-use muzanci_git::get_remote;
+use muzanci_git::GitClient;
+use muzanci_interpreter::StepConfig;
 use muzanci_transport::message::DebugClientMessage;
 use muzanci_transport::message::DebugConfig;
 use muzanci_transport::message::DebugId;
@@ -23,15 +26,15 @@ pub fn run_debug_session(job: JobConfig) -> anyhow::Result<()> {
     rt.block_on(async {
         let cancellation_token = CancellationToken::new();
 
-        let hostname = "localhost:8002";
-        let mux_handle = connect(hostname, cancellation_token.clone()).await?;
-
         let remote = {
             // TODO: Refactor these to be CLI options.
-            let target_dir = PathBuf::from(".");
+            let target_dir = PathBuf::from("./.git");
             let remote_name = "origin";
-            get_remote(&target_dir, remote_name)?
+            GitClient::try_default()?.get_remote(&target_dir, remote_name)?
         };
+
+        let hostname = "localhost:8002";
+        let mux_handle = connect(hostname, cancellation_token.clone()).await?;
 
         let capacity = 1;
         let debug_config = DebugConfig {
@@ -183,13 +186,9 @@ impl DebugClient {
         self.start_diff_upload().await?;
         self.send_diff().await?;
         self.complete_diff_upload().await?;
+        self.debugger_apply_diff().await?;
+        self.debugger_control().await?;
 
-        for step in self.debug_config.job.steps {
-            // Prompt user for
-            // "s" to execute step
-            // "d" to start shell
-            // "q" to quit
-        }
         Ok(())
     }
 
@@ -236,27 +235,320 @@ impl DebugClient {
     }
 
     #[tracing::instrument(skip_all)]
-    async fn checkout_debugger(&self) -> anyhow::Result<()> {
+    async fn checkout_debugger(&mut self) -> anyhow::Result<()> {
+        self.channel_tx
+            .send(Message::DebugClient(
+                DebugClientMessage::CheckoutDebuggerRequest,
+            ))
+            .await?;
+
+        self.channel_rx
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("Channel closed"))
+            .and_then(|response| match response {
+                Message::DebugClient(DebugClientMessage::CheckoutDebuggerResponse { result }) => {
+                    result.map_err(|e| anyhow::anyhow!(e))
+                }
+                _ => Err(anyhow::anyhow!("Unexpected message type")),
+            })
+    }
+
+    #[tracing::instrument(skip_all)]
+    async fn create_diff(&mut self) -> anyhow::Result<()> {
         Ok(())
     }
 
     #[tracing::instrument(skip_all)]
-    async fn create_diff(&self) -> anyhow::Result<()> {
+    async fn start_diff_upload(&mut self) -> anyhow::Result<()> {
+        self.channel_tx
+            .send(Message::DebugClient(
+                DebugClientMessage::StartDiffUploadRequest,
+            ))
+            .await?;
+
+        self.channel_rx
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("Channel closed"))
+            .and_then(|response| match response {
+                Message::DebugClient(DebugClientMessage::StartDiffUploadResponse { result }) => {
+                    result.map_err(|e| anyhow::anyhow!(e))
+                }
+                _ => Err(anyhow::anyhow!("Unexpected message type")),
+            })
+    }
+
+    #[tracing::instrument(skip_all)]
+    async fn send_diff(&mut self) -> anyhow::Result<()> {
+        self.channel_tx
+            .send(Message::DebugClient(
+                DebugClientMessage::UploadDiffChunkRequest,
+            ))
+            .await?;
+
+        self.channel_rx
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("Channel closed"))
+            .and_then(|response| match response {
+                Message::DebugClient(DebugClientMessage::UploadDiffChunkResponse { result }) => {
+                    result.map_err(|e| anyhow::anyhow!(e))
+                }
+                _ => Err(anyhow::anyhow!("Unexpected message type")),
+            })
+    }
+
+    #[tracing::instrument(skip_all)]
+    async fn complete_diff_upload(&mut self) -> anyhow::Result<()> {
+        self.channel_tx
+            .send(Message::DebugClient(
+                DebugClientMessage::CompleteDiffUploadRequest,
+            ))
+            .await?;
+
+        self.channel_rx
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("Channel closed"))
+            .and_then(|response| match response {
+                Message::DebugClient(DebugClientMessage::CompleteDiffUploadResponse { result }) => {
+                    result.map_err(|e| anyhow::anyhow!(e))
+                }
+                _ => Err(anyhow::anyhow!("Unexpected message type")),
+            })
+    }
+
+    #[tracing::instrument(skip_all)]
+    async fn debugger_apply_diff(&mut self) -> anyhow::Result<()> {
+        self.channel_tx
+            .send(Message::DebugClient(
+                DebugClientMessage::DebuggerApplyDiffRequest,
+            ))
+            .await?;
+
+        self.channel_rx
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("Channel closed"))
+            .and_then(|response| match response {
+                Message::DebugClient(DebugClientMessage::DebuggerApplyDiffResponse { result }) => {
+                    result.map_err(|e| anyhow::anyhow!(e))
+                }
+                _ => Err(anyhow::anyhow!("Unexpected message type")),
+            })?;
         Ok(())
     }
 
     #[tracing::instrument(skip_all)]
-    async fn start_diff_upload(&self) -> anyhow::Result<()> {
+    async fn debugger_control(&mut self) -> anyhow::Result<()> {
+        let mut current_step: usize = 0;
+        let mut step_success: Vec<Option<bool>> = vec![None; self.debug_config.job.steps.len()];
+        let mut input = String::new();
+        loop {
+            // print current state
+            println!("Job: {}", self.debug_config.job.name);
+            println!("Steps:");
+            for (i, step) in self.debug_config.job.steps.iter().enumerate() {
+                let status = if let Some(success) = step_success[i] {
+                    if success { "✔" } else { "✘" }
+                } else {
+                    "?"
+                };
+                let cursor = if i == current_step { ">" } else { " " };
+                println!(
+                    "{} {} ({}) {}\n\t\"{}\"\n",
+                    cursor,
+                    status,
+                    i + 1,
+                    step.name,
+                    step.command
+                );
+            }
+
+            println!("You can open a (s)hell, (m)ove, (n)ext, (c)ontinue, or (q)uit.");
+
+            print!("> ");
+            use std::io::Write;
+            std::io::stdout().flush()?;
+
+            // read user input line
+            input.clear();
+            std::io::stdin().read_line(&mut input)?;
+
+            // parse into command
+            let mut parts = input.trim().split_whitespace();
+            let command = parts.next();
+            let command = match command {
+                Some(command) => match command {
+                    "s" | "shell" => {
+                        if let Some(i) = parts.next().and_then(|s| s.parse::<usize>().ok()) {
+                            DebuggerCommand::Ssh { step_idx: i - 1 }
+                        } else {
+                            DebuggerCommand::Ssh {
+                                step_idx: current_step,
+                            }
+                        }
+                    }
+                    "m" | "move" => {
+                        if let Some(i) = parts.next().and_then(|s| s.parse::<usize>().ok()) {
+                            DebuggerCommand::Move { step_idx: i - 1 }
+                        } else {
+                            DebuggerCommand::Move {
+                                step_idx: current_step,
+                            }
+                        }
+                    }
+                    "c" | "continue" => {
+                        if let Some(i) = parts.next().and_then(|s| s.parse::<usize>().ok()) {
+                            DebuggerCommand::Continue { step_idx: i - 1 }
+                        } else {
+                            DebuggerCommand::Continue {
+                                step_idx: current_step,
+                            }
+                        }
+                    }
+                    "n" | "next" => DebuggerCommand::Next,
+                    "q" | "quit" => DebuggerCommand::Quit,
+                    _ => {
+                        println!("Unknown command: {}", input.trim());
+                        continue;
+                    }
+                },
+                None => continue,
+            };
+
+            // execute command
+            match command {
+                DebuggerCommand::Quit => break,
+                DebuggerCommand::Next => {
+                    println!("Executing next.");
+                    // Execute the step pointed by current_step.
+                    let step = self
+                        .debug_config
+                        .job
+                        .steps
+                        .get(current_step)
+                        .unwrap()
+                        .clone();
+                    let exit_code = self.debugger_execute_step(step).await?;
+                    if exit_code == ExitCode::SUCCESS {
+                        step_success[current_step] = Some(true);
+                        if current_step + 1 < self.debug_config.job.steps.len() {
+                            current_step += 1;
+                        }
+                    } else {
+                        step_success[current_step] = Some(false);
+                    }
+                }
+                DebuggerCommand::Continue { step_idx } => {
+                    println!("Executing continue on {}", step_idx);
+                    // Starting with step_idx, execute each step until the end or a failure.
+                    current_step = step_idx;
+                    loop {
+                        if current_step >= self.debug_config.job.steps.len() {
+                            break;
+                        }
+
+                        let step = self
+                            .debug_config
+                            .job
+                            .steps
+                            .get(current_step)
+                            .unwrap()
+                            .clone();
+                        let exit_code = self.debugger_execute_step(step).await?;
+                        if exit_code == ExitCode::SUCCESS {
+                            step_success[current_step] = Some(true);
+                            if current_step + 1 < self.debug_config.job.steps.len() {
+                                current_step += 1;
+                            } else if current_step + 1 == self.debug_config.job.steps.len() {
+                                break;
+                            }
+                        } else {
+                            step_success[current_step] = Some(false);
+                            // If a failure occurs, do not advance the current_step.
+                            break;
+                        }
+                    }
+                }
+                DebuggerCommand::Ssh { step_idx } => {
+                    println!("Executing ssh on {}", step_idx);
+                    // Open an interactive shell for the step pointed by step_idx.
+                    if step_idx < self.debug_config.job.steps.len() {
+                        current_step = step_idx;
+                        let step = self
+                            .debug_config
+                            .job
+                            .steps
+                            .get(current_step)
+                            .unwrap()
+                            .clone();
+                        self.debugger_ssh_step(step).await?;
+                    }
+                }
+                DebuggerCommand::Move { step_idx } => {
+                    println!("Executing move on {}", step_idx);
+                    // Move current_step to step_idx.
+                    if step_idx < self.debug_config.job.steps.len() {
+                        current_step = step_idx;
+                    } else {
+                        println!("Step {} is out of bounds.", step_idx);
+                    }
+                }
+            }
+
+            // update state
+        }
+
         Ok(())
     }
 
     #[tracing::instrument(skip_all)]
-    async fn send_diff(&self) -> anyhow::Result<()> {
-        Ok(())
+    async fn debugger_execute_step(&mut self, step: StepConfig) -> anyhow::Result<ExitCode> {
+        self.channel_tx
+            .send(Message::DebugClient(
+                DebugClientMessage::DebuggerExecuteStepRequest { step: step },
+            ))
+            .await?;
+        self.channel_rx
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("Channel closed"))
+            .and_then(|response| match response {
+                Message::DebugClient(DebugClientMessage::DebuggerExecuteStepResponse {
+                    result,
+                }) => result.map_err(|e| anyhow::anyhow!(e)),
+                _ => Err(anyhow::anyhow!("Unexpected message type")),
+            })?;
+        Ok(ExitCode::SUCCESS)
     }
 
     #[tracing::instrument(skip_all)]
-    async fn complete_diff_upload(&self) -> anyhow::Result<()> {
+    async fn debugger_ssh_step(&mut self, step: StepConfig) -> anyhow::Result<()> {
+        self.channel_tx
+            .send(Message::DebugClient(
+                DebugClientMessage::DebuggerExecuteStepRequest { step: step },
+            ))
+            .await?;
+        self.channel_rx
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("Channel closed"))
+            .and_then(|response| match response {
+                Message::DebugClient(DebugClientMessage::DebuggerExecuteStepResponse {
+                    result,
+                }) => result.map_err(|e| anyhow::anyhow!(e)),
+                _ => Err(anyhow::anyhow!("Unexpected message type")),
+            })?;
         Ok(())
     }
+}
+
+pub enum DebuggerCommand {
+    Quit,
+    Next,
+    Continue { step_idx: usize },
+    Ssh { step_idx: usize },
+    Move { step_idx: usize },
 }
